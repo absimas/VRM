1454f9ee4fe63c68d06e3c6cac54cd579d59709a
Simas <simas@insight.co>
2017-03-04 20:51:54 +0200

    Implement a Word - char[5].

--- src/vrm/Word.java
+++ src/vrm/Word.java
@@ -1,27 +1,28 @@
 package vrm;
 
 /**
- * {@link String} wrapper that forces a length of {@link #WORD_LENGTH}.
+ * Word with a length of {@link #WORD_LENGTH}.
  */
-public class WordWrapper {
+public class Word {
 
   private static final int WORD_LENGTH = 5;
 
-  public final String word;
+  public final char[] symbols;
 
   /**
-   * Creates a word with 5 words.
+   * Creates a word with 5 symbols.
    * @param word 5 symbol word
    */
-  public WordWrapper(String word) {
+  public Word(String word) {
     if (word == null || word.length() != WORD_LENGTH) {
       throw new IllegalArgumentException("Word must be of length 5. Got: " + word);
     }
-    this.word = word;
+    this.symbols = word.toCharArray();
   }
 
   @Override
   public String toString() {
-    return word;
+    return new String(symbols);
   }
+
 }

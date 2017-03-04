7eddc65957e9ef79ed2b1d40915905974adf4932
Simas <ab.simas@gmail.com>
2017-03-04 20:56:29 +0200

    Represent memory in VM as a 100 words passed by the creator.

--- src/vrm/VirtualMachine.java
+++ src/vrm/VirtualMachine.java
@@ -1,9 +1,16 @@
 package vrm;
 
+import com.sun.istack.internal.NotNull;
+
 /**
  * Created by Simas on 2017 Mar 04.
  */
 public class VirtualMachine {
+
+  /**
+   * Real machine memory size in words.
+   */
+  public static final int VM_MEMORY_SIZE = 100;
 
   /**
    * Temporary. Size 5 bytes.
@@ -17,5 +24,22 @@ public class VirtualMachine {
    * Comparison result. Size 1 byte.
    */
   public Comparison C = Comparison.EQUAL;
+
+  /**
+   * Memory in words. The size is determined by {@link #VM_MEMORY_SIZE}.
+   */
+  private final Word[] MEMORY;
+
+  /**
+   *
+   * @param memory
+   */
+  public VirtualMachine(@NotNull Word[] memory) {
+    if (memory.length != VM_MEMORY_SIZE) {
+      throw new IllegalArgumentException("VM memory must be of size " + VM_MEMORY_SIZE);
+    }
+
+    MEMORY = memory;
+  }
 
 }

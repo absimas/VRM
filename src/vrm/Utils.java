20a4bd2377d779ca34f1c16183624af9aeae8c81
Simas <ab.simas@gmail.com>
2017-03-12 20:53:14 +0200

    Add convenience method that precedes zeroes to a string/number to meet the wanted size.

--- src/vrm/Utils.java
+++ src/vrm/Utils.java
@@ -14,4 +14,27 @@ public class Utils {
     return (value & flag) == flag;
   }
 
+  /**
+   * Precedes the given string with zeroes until the wanted size is met.
+   * Note that if the given string is longer, it will be trimmed to meet the size and no zeroes will be added.
+   * E.g.
+   * <code>
+   *   precedeZeroes("abc", 5);
+   * </code>
+   * will return 00abc.
+   * @param string  string that will be preceded by zeroes
+   * @param size    total size of the resulting string
+   * @return string preceded with zeroes to meet the wanted size.
+   */
+  public static String precedeZeroes(String string, int size) {
+    return ("0000000000" + string).substring(size);
+  }
+
+  /**
+   * @see #precedeZeroes(String, int)
+   */
+  public static String precedeZeroes(int integer, int size) {
+    return precedeZeroes(String.valueOf(integer), size);
+  }
+
 }

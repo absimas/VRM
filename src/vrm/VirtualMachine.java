b8939630bece1bd14cb6be6c03cf42b01a0847b2
Simas <ab.simas@gmail.com>
2017-03-15 17:17:59 +0200

    Modify VM ToDos

--- src/vrm/VirtualMachine.java
+++ src/vrm/VirtualMachine.java
@@ -27,10 +27,10 @@ public class VirtualMachine extends Machine {
   @Override
   public void execute(Command command) throws UnhandledCommandException, MemoryOutOfBoundsException {
     switch (command.type) {
-      // Commands that require super privileges - throw
+      // Commands that require super privileges - throw // ToDo note that the command arguments wll need to be converted using PLR
       case GD: case PD: case RD: case WD: case SD: case HALT:
         throw new UnhandledCommandException(command, String.format("Command %s wasn't handled in %s.", command, this));
-      // ToDo can we forbid these commands in a VM?
+      // ToDo can we forbid the following commands in a VM?
       case GT: case PT: case STVM: case SVRG: case LDRG:
         throw new UnhandledCommandException(command, String.format("Command %s shouldn't be handled virtually.", command));
       // Other commands invoke the default handling
